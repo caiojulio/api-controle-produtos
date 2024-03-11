faa911af4a4d02b34f6024b56498023074274f75
caiojulio <caiojuliosilv@gmail.com>
2024-03-11 18:41:50 -0300

    finalização da API REST

--- springboot_michelli_brito/springboot/src/main/java/com/example/springboot/controllers/ProductController.java
+++ springboot_michelli_brito/springboot/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -7,11 +7,14 @@ import com.example.springboot.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -19,9 +22,52 @@ public class ProductController {
     ProductRepository productRepository;
 
     @PostMapping("/products") //mapeamento via HTTP para postagem de dados na base de dados
-    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel); // faz a conversão do dto para o model que será guardado na base de dados
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel)); //resposta de criação e utilização do método save do JPA Repository para executar operação de guardar dados no banco de dados vinculado.
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product;
+        product = productRepository.findById(id);
+
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductRecordDto productRecordDto) {
+        Optional<ProductModel> product;
+        product = productRepository.findById(id);
+
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found");
+        }
+
+        ProductModel productModel = product.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product;
+        product = productRepository.findById(id);
+
+        if (product.isEmpty()) { //verifica se o produto existe
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found");
+        }
+
+        productRepository.deleteById(id); //deleta produto usando repositorio JPA usando a função deleteById
+        return ResponseEntity.status(HttpStatus.OK).body("Product: " + id + " deleted");
     }
 }
